d969549f1e7e28a5f37de151fa52a9c3cc00ccce
Dipesh  Pandey <dipeshpandey@Dipeshs-MacBook-Air.local>
2023-09-16 19:42:05 +0200

    deploy dev for now

--- radionovacms/settings/dev.py
+++ radionovacms/settings/dev.py
@@ -11,6 +11,17 @@ ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", default="postgres"),
+        "USER": os.environ.get("POSTGRES_USER", default="radionova"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", default="R@dionova123"),
+        "HOST": os.environ.get("POSTGRES_HOST", default="radionova.postgres.database.azure.com"),
+        "PORT": 5432,
+    }
+}
+
 
 try:
     from .local import *
