b43e3cf2d2821b44c1681c731ef34a842bd327c4
Dipesh <dipesh.pandey@nofence.no>
2024-09-05 22:32:50 +0200

    add domain

--- radionova/settings.py
+++ radionova/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['radionova-aecnh6e8gjh8cmfb.eastus-01.azurewebsites.net',
+ALLOWED_HOSTS = ['radionova.azurewebsites.net',
                  'localhost1', 'localhost', '127.0.0.1', ]
 
 
