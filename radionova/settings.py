957d94cdee9b1664de27ea76f842fe35ff26058b
Radi Halabi <radihalabi2010@hotmail.com>
2025-02-07 19:27:01 +0100

    Root page click on logo
    Navigation back to hjemmeside

--- radionova/settings.py
+++ radionova/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
         'PASSWORD': os.environ.get('DBPASS'),
         'PORT': os.environ.get('DBPORT'),
         'OPTIONS': {
-            'sslmode': 'require',
+             'sslmode': 'disable',
         },
 
         
--- tears/templatetags/navigation_tags.py
+++ tears/templatetags/navigation_tags.py
@@ -7,3 +7,7 @@ register = template.Library()
 def get_navbar_pages():
     return Page.objects.live().public().in_menu().filter(depth__gt=2)
     #return ['Item 1', 'Item 2', 'item 3']
+    
+@register.simple_tag
+def get_root_page():
+    return Page.objects.live().public().in_menu().first()
