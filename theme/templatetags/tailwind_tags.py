c1a08637c04309b05864f5b4581be14a6a905d8d
dipespandey <pandey.dipesh50@gmail.com>
2025-05-06 19:16:16 +0200

    use static tag

--- theme/templatetags/tailwind_tags.py
+++ theme/templatetags/tailwind_tags.py
@@ -1,10 +1,11 @@
 from django import template
 from django.utils.safestring import mark_safe
+from django.templatetags.static import static
 
 register = template.Library()
 
 @register.simple_tag
 def tailwind_css():
     return mark_safe(
-        '<link rel="stylesheet" href="/static/css/tailwind.css">'
+        f'<link rel="stylesheet" href="{static("css/tailwind.css")}">'
     ) 
